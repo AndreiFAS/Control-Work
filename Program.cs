7df53b2d06cdd0a4e27aadfbfd2b7cd64a66c557
ApkoH <khuzdul@bk.ru>
2022-05-10 18:36:51 +0300

    add code v0.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина 
+// которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать 
+// на старте выполнения алгоритма.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "2", "computer"] -> ["2"]
+// ["Russia", "Germany", "Moscow"] -> []
+
+string[] arrayInput = new string[] { "hello", "2", "world", ":-)" };
+
+string[] CreateValidArray(string[] array)
+{
+    int index = 0;
+    int count = 0;
+    int len = array.Length;
+    while (index < len)
+    {
+        if (array[index].Length <= 3) count++;
+        index++;
+    }
+    string[] validArray = new string[count];
+    int validIndex = 0;
+    index = 0;
+    while (index < len)
+    {
+        if (array[index].Length <= 3) validArray[validIndex++] = array[index];
+        index++;
+    }
+    return validArray;
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+PrintArray(arrayInput);
+Console.WriteLine();
+PrintArray(CreateValidArray(arrayInput));
